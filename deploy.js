f808a7d68b8e56f3da6f89060ecaad59f7862112
Vishal Sutariya <SVishuS@1998>
2022-04-22 13:00:34 +0530

    deploy contract to Rinkeby network

--- deploy.js
+++ deploy.js
@@ -1,1 +1,27 @@
-// deploy code will go here
+// deploy code will go here for deploy contract on Rinkeby network
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'spread lawn decade okay faint twist illness escape deny ramp ring letter',
+    'https://rinkeby.infura.io/v3/18a6c04411544bdbaa8ed773dd17459c'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account --> ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello World!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to --> ', result.options.address);
+
+    provider.engine.stop();
+};
+
+deploy();
