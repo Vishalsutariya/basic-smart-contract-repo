d5bb8044d9d13bb7e083428f4670ede6b6e0dbbc
Vishal Sutariya <SVishuS@1998>
2022-04-22 09:56:04 +0530

    deploy contract in test file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,20 +4,24 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
-beforeEach(() => {
+let accounts;
+let inbox;
+beforeEach(async () => {
 
     //get a list of accounts from ganache
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        });
+    accounts = await web3.eth.getAccounts();
 
+    //Use one of those account to deploy contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hello World!'] })
+        .send({ from: accounts[0], gas: '1000000' })
 
 })
 
 describe('Inbox', () => {
     it('deploy a contract', () => {
-
+        console.log(inbox);
     });
 });
